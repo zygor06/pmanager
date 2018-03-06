69f84b8f10bbd1aa2758ed0614577277b1ce0d9b
hygor christian dias <hygor_christian@hotmail.com>
2018-03-06 07:18:29 -0300

    #001 - Creating models and migrations.

---
+++ database/migrations/2018_03_06_093244_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::enableForeignKeyConstraints();
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->longText('description');
+            $table->foreign('company_id')->references('id')->on('company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
